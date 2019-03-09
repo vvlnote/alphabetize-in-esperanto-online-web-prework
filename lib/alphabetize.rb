16b191c03b9960f1cf77761598e656fb0fa35bc4
Joy Lin <vvlnote@hotmail.com>
2019-03-09 19:46:27 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
 def alphabetize(arr)
   # code here
+  experanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  sorted_arr = arr
+  i = 0
+  while sorted_arr == arr
+    sorted_arr = arr.sort_by {|str|
+      experanto_alphabet.index(str[i])
+    } 
+    i += 1
+  end
+  sorted_arr
 end
