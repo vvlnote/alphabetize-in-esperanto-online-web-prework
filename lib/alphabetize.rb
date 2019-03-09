5cc8c065abd857bdcadc7b5779eeb6f5d4fcf00a
Joy Lin <vvlnote@hotmail.com>
2019-03-09 19:42:27 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   # code here
   experanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by {|str|
-    experanto_alphabet[str[0]]
+    experanto_alphabet.index(str[0])
   }
   arr
 end
